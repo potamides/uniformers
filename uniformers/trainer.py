510619002bc7029f5ae68b6b8db9345adad8b643
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-06-17 22:29:22 +0200

    feat: scale batchsize according to gradient accumulation steps

--- uniformers/trainer.py
+++ uniformers/trainer.py
@@ -30,33 +30,30 @@ class LMTrainingArguments(TrainingArguments):
     TrainingArguments use same batch_size on each GPU. (see
     https://discuss.pytorch.org/t/should-we-split-batch-size-according-to-ngpu-per-node-when-distributeddataparallel/72769/15)
     This should also work for DataParallel which does splitting on its own (see
-    https://discuss.pytorch.org/t/a-question-concerning-batchsize-and-multiple-gpus-in-pytorch/33767)
+    https://discuss.pytorch.org/t/a-question-concerning-batchsize-and-multiple-gpus-in-pytorch/33767).
+    Additionally, batch_size is scaled according to gradient accumulation
+    steps.
     """
 
     def __init__(self, global_train_batch_size=8, global_eval_batch_size=8, **kwargs):
-        kwargs["per_device_train_batch_size"] = global_train_batch_size
-        kwargs["per_device_eval_batch_size"] = global_eval_batch_size
         super().__init__(**kwargs)
+        self.per_device_train_batch_size = self._scale_batch_size(
+            global_train_batch_size
+        )
+        self.per_device_eval_batch_size = self._scale_batch_size(global_eval_batch_size)
         if self.world_size > 1:
             logger.info(f"Dividing batches equally on {self.world_size} processes.")
 
-    @property
-    def train_batch_size(self) -> int:
-        train_batch_size, remainder = divmod(
-            self.per_device_train_batch_size, self.world_size
+    def _scale_batch_size(self, batch_size) -> int:
+        scaled_batch_size, remainder = divmod(
+            batch_size,
+            self.world_size * self.gradient_accumulation_steps,
         )
         if remainder != 0:
-            raise ValueError("`batch_size` must be divisible by number of processes.")
-        return train_batch_size
-
-    @property
-    def eval_batch_size(self) -> int:
-        eval_batch_size, remainder = divmod(
-            self.per_device_eval_batch_size, self.world_size
-        )
-        if remainder != 0:
-            raise ValueError("`batch_size` must be divisible by number of processes.")
-        return eval_batch_size
+            raise ValueError(
+                "`batch_size` must be divisible by number of processes times gradient accumulation steps."
+            )
+        return scaled_batch_size
 
 
 class LMTrainer(Trainer):
