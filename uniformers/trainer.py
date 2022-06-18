00bdaf4bea95762a47dbfff5abec1b75a77dae89
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-06-18 12:05:51 +0200

    perf: improve dataset loading for big datasets

--- uniformers/trainer.py
+++ uniformers/trainer.py
@@ -4,8 +4,9 @@ from os.path import isdir, isfile, join
 from random import randrange
 from textwrap import shorten
 
-from datasets.load import load_dataset
+from datasets import Features, Sequence, Value
 from datasets.fingerprint import update_fingerprint
+from datasets.load import load_dataset
 from transformers.data.data_collator import DataCollatorForLanguageModeling
 from transformers.trainer import Trainer
 from transformers.trainer_utils import get_last_checkpoint
@@ -34,8 +35,17 @@ class LMTrainingArguments(TrainingArguments):
     steps.
     """
 
-    def __init__(self, global_train_batch_size=8, global_eval_batch_size=8, **kwargs):
+    def __init__(
+        self,
+        global_train_batch_size=8,
+        global_eval_batch_size=8,
+        eval_samples=1000,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
+        self.global_train_batch_size = global_train_batch_size
+        self.global_eval_batch_size = global_eval_batch_size
+        self.eval_samples = eval_samples
         self.per_device_train_batch_size = self._scale_batch_size(
             global_train_batch_size
         )
@@ -79,35 +89,9 @@ class LMTrainer(Trainer):
         self.model = model
         self.tokenizer = tokenizer
         self.sequence_length = sequence_length
-
-        dataset_name = "stas/openwebtext-10k" if test_run else "the_pile_openwebtext2"
-        raw_dataset = load_dataset(dataset_name, split="train")
-        # internal hash computation is not the same across sessions so we have
-        # to compute it ourselves
-        new_fingerprint = update_fingerprint(
-            raw_dataset._fingerprint, # pyright: ignore
-            self.__class__.tokenize,
-            {
-                "tokenizer": tokenizer,
-                "sequence_length": self.sequence_length,
-                "split": self.__class__.split,
-            },
-        )
-        tokenized_dataset = raw_dataset.map(
-            self.tokenize,
-            batched=True,
-            new_fingerprint=new_fingerprint,  # pyright: ignore
-            remove_columns=raw_dataset.column_names,  # pyright: ignore
-        )
-        logger.info(f"Original dataset length: {len(raw_dataset)}")  # pyright: ignore
-        logger.info(f"Tokenized dataset length: {len(tokenized_dataset)}")
-        index = randrange(len(tokenized_dataset))
-        logger.info(shorten(f"Sample {index} of the training set: {tokenized_dataset[index]}.", width=120))
-
-        split_dataset = tokenized_dataset.train_test_split(test_size=0.005)
         data_collator = DataCollatorForLanguageModeling(self.tokenizer, mlm=False)
 
-        if self.parameters < 350 * 10**6:
+        if self.parameters < 280 * 10**6:
             learning_rate = "base"
         elif self.parameters < 770 * 10**6:
             learning_rate = "medium"
@@ -120,12 +104,12 @@ class LMTrainer(Trainer):
         )
 
         # interesting resource: https://huggingface.co/course/chapter7/6?fw=pt
-        args = LMTrainingArguments(
+        self.args = LMTrainingArguments(
             optim="adamw_torch",
             lr_scheduler_type="cosine",
             learning_rate=LM_LEARNING_RATES[learning_rate],
-            num_train_epochs=1,
-            max_steps=1000 if test_run else (100_000 if transfer else 600_000),
+            max_steps=1000 if test_run else (50_000 if transfer else 600_000), # ~25Gb of text (for transfer)
+            eval_samples=200 if test_run else 102_400, # 100Mb of text
             weight_decay=0.1,
             warmup_ratio=0.01,
             global_train_batch_size=512,
@@ -139,26 +123,88 @@ class LMTrainer(Trainer):
             gradient_checkpointing=gradient_checkpointing,
             ddp_find_unused_parameters=False,
             evaluation_strategy="no" if test_run else "steps",
-            eval_steps=50000,
-            logging_steps=test_run and 20 or 1000,
-            save_steps=10000,
+            eval_steps=2500,
+            logging_steps=20 if test_run else 250,
+            logging_first_step=True,
+            save_steps=2500,
             output_dir=output_dir,
         )
+
+        split_dataset = self.load_dataset(
+            "stas/openwebtext-10k" if test_run else "the_pile_openwebtext2"
+        )
+
         super().__init__(
             model=self.model,
             tokenizer=self.tokenizer,
-            args=args,
+            args=self.args,
             data_collator=data_collator,
             train_dataset=split_dataset["train"],  # pyright: ignore
             eval_dataset=split_dataset["test"],  # pyright: ignore
             **kwargs,
         )
 
+    def load_dataset(self, dataset_name):
+        features = (
+            Features(
+                {
+                    "title": Value("string"),
+                    "text": Value("string"),
+                    "reddit_scores": Sequence(Value("int32")),
+                }
+            )
+            if dataset_name == "the_pile_openwebtext2"  # column types need special care for this dataset
+            else None
+        )
+        # the minimum amount of samples we need for training
+        self.min_num_samples = (
+            self.args.max_steps * self.args.global_train_batch_size
+            + self.args.eval_samples
+        )
+        self._num_samples = 0
+        raw_dataset = load_dataset(dataset_name, split="train", features=features)
+        # internal hash computation is not the same across sessions so we have
+        # to compute it ourselves
+        new_fingerprint = update_fingerprint(
+            raw_dataset._fingerprint,  # pyright: ignore
+            self.__class__.tokenize,
+            {
+                "tokenizer": self.tokenizer,
+                "sequence_length": self.sequence_length,
+                "split": self.__class__.split,
+                "min_num_samples": self.min_num_samples,
+            },
+        )
+        # we only load the number of samples we need to save some disk space
+        logger.info("Tokenizing dataset, progress reports may be inaccurate (upper bound).")
+        tokenized_dataset = raw_dataset.map(
+            self.tokenize,
+            batched=True,
+            new_fingerprint=new_fingerprint,  # pyright: ignore
+            remove_columns=raw_dataset.column_names,  # pyright: ignore
+        )
+        split_dataset = tokenized_dataset.train_test_split(
+            test_size=self.args.eval_samples
+        )
+        logger.info(f"Original dataset length: {len(raw_dataset)}")  # pyright: ignore
+        logger.info(
+            f"Tokenized dataset length: {len(split_dataset['train'])} (train)"
+            f", {len(split_dataset['test'])} (test)"
+        )
+        index = randrange(len(split_dataset["train"]))
+        logger.info(
+            shorten(
+                f"Sample {index} of the training set: {split_dataset['train'][index]}.",
+                width=120,
+            )
+        )
+        return split_dataset
+
     def train(self, **kwargs):
         if not self.args.overwrite_output_dir and isdir(self.args.output_dir):
             last_checkpoint = get_last_checkpoint(self.args.output_dir)
 
-            if isfile(join(self.args.output_dir, 'config.json')):
+            if isfile(join(self.args.output_dir, "config.json")):
                 logger.warning(
                     f"Output directory ({self.args.output_dir}) exists already and is not empty. Skipping training."
                 )
@@ -172,15 +218,17 @@ class LMTrainer(Trainer):
         return super().train(resume_from_checkpoint=last_checkpoint, **kwargs)
 
     def tokenize(self, element):
-        outputs = self.tokenizer(element["text"])
         input_batch = []
+        if self._num_samples < self.min_num_samples:
+            outputs = self.tokenizer(element["text"])
 
-        for input_ids in chain.from_iterable(
-            self.split(ids, self.sequence_length)
-            for ids in outputs["input_ids"]  # pyright: ignore
-        ):
-            if len(input_ids) == self.sequence_length:
-                input_batch.append(input_ids)
+            for input_ids in chain.from_iterable(
+                self.split(ids, self.sequence_length)
+                for ids in outputs["input_ids"]  # pyright: ignore
+            ):
+                if len(input_ids) == self.sequence_length:
+                    input_batch.append(input_ids)
+            self._num_samples = self._num_samples + len(input_batch)
         return {"input_ids": input_batch}
 
     def split(self, lst, n):
@@ -191,25 +239,3 @@ class LMTrainer(Trainer):
     @cached_property
     def parameters(self):
         return sum(t.numel() for t in self.model.parameters())
-
-    #@classmethod
-    #def from_pretrained(cls, output_dir):
-    #    if isfile(join(output_dir, "config.json")):  # pyright: ignore
-    #        logger.info(
-    #            f"Output directory ({output_dir}) exists and is not empty. "
-    #            "Returning pretrained models."
-    #        )
-
-    #        config = AutoConfig.from_pretrained(
-    #            output_dir,
-    #        )
-    #        tokenizer = AutoTokenizer.from_pretrained(
-    #            output_dir,
-    #        )
-    #        model = AutoModelForSeq2SeqLM.from_pretrained(
-    #            output_dir,
-    #            config=config,
-    #        )
-    #        return model, tokenizer
-    #    else:
-    #        raise FileNotFoundError
