87691c7894003519a6f3bc9d386ef911eb395077
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-06-21 15:05:04 +0200

    build: configure setuptools using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="uniformers",
-    description="Universal Coded Character Set Transformers for Language Modelling",
-    keywords=[
-        "Deep Learning",
-        "Language Models",
-        "Transformers",
-        "Character-Level",
-        "Tokenization-Free",
-        "Natural Language Processing",
-        "Natural Language Generation",
-        "Poetry Generation",
-    ],
-    url="https://github.com/potamides/uniformers",
-    author="Jonas Belouadi",
-    author_email="potamides@posteo.net",
-    packages=find_packages(include=["uniformers*"]),
-    install_requires=[
-        "datasets~=2.3.2",
-        "numpy~=1.22.4",
-        "tokenizers~=0.12.1",
-        "torch~=1.11.0",
-        "zstandard~=0.17.0",
-        "transformers==4.20.0",
-    ],
-    extras_require={
-        "examples": [],
-    },
-    python_requires="~=3.10.4",
-    zip_safe=False,
-)
+# For the time being, pip still might require a setup.py file to support
+# editable installs (see
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html).
+setup()
