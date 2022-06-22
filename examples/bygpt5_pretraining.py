29ea675e2a57d837cf42419e2637238d3bbd0976
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-06-22 18:41:39 +0200

    feat: use argparse to supply arguments to scripts

--- examples/bygpt5_pretraining.py
+++ examples/bygpt5_pretraining.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env -S python -m torch.distributed.launch --nproc_per_node gpu
-from os import getenv
+from argparse import ArgumentParser
 from os.path import join
+from os.path import basename
 
+from transformers.trainer_utils import set_seed
 from transformers.utils.logging import (
     enable_explicit_format,
-    set_verbosity_info,
     get_logger,
+    set_verbosity_info,
 )
 
 from uniformers.models.bygpt5 import ByGPT5Config, ByGPT5LMHeadModel, ByGPT5Tokenizer
 from uniformers.trainer import LMTrainer
-from transformers.trainer_utils import set_seed
-
-from os.path import basename
 
 set_verbosity_info()
 enable_explicit_format()
 logger = get_logger("transformers")
 set_seed(0)
 
-size = getenv("UNIFORMERS_MODEL_SIZE", "small")
-output_dir = join(getenv("UNIFORMERS_DATA", ""), f"models/bygpt5-{size}")
-base_model = f"google/byt5-{size}"
 
-
-def train(from_scratch=False, gradient_accumulation_steps=8, gradient_checkpointing=False):
+def train(
+    base_model,
+    output_dir,
+    from_scratch=False,
+    gradient_accumulation_steps=8,
+    gradient_checkpointing=False,
+):
     directory = f"{output_dir}-scratch" if from_scratch else output_dir
     logger.info(
         f"Training {basename(base_model)}-style model from {'scratch' if from_scratch else 'checkpoint'}."
@@ -40,7 +41,7 @@ def train(from_scratch=False, gradient_accumulation_steps=8, gradient_checkpoint
         else:
             model = ByGPT5LMHeadModel.from_pretrained(base_model)
         # T5 doesn't support both cache and grad checkpointing at the same time
-        model.config.use_cache = not gradient_checkpointing # pyright: ignore
+        model.config.use_cache = not gradient_checkpointing  # pyright: ignore
         tokenizer = ByGPT5Tokenizer.from_pretrained(base_model)
         trainer = LMTrainer(
             model,
@@ -48,7 +49,7 @@ def train(from_scratch=False, gradient_accumulation_steps=8, gradient_checkpoint
             directory,
             gradient_accumulation_steps=gradient_accumulation_steps,
             gradient_checkpointing=gradient_checkpointing,
-            #test_run=True,
+            # test_run=True,
             transfer=True,
         )
         trainer.train()
@@ -57,8 +58,44 @@ def train(from_scratch=False, gradient_accumulation_steps=8, gradient_checkpoint
     return model, tokenizer
 
 
+def byt5_size_to_bygpt5_size(size):
+    """
+    Since we only use the decoder our models are a lot smaller which is why we
+    use more appropriate names.
+    """
+    match size:
+        case "large":
+            return "medium"
+        case "xl":
+            return "large"
+        case "xxl":
+            return "xl"
+        case _:
+            return size
+
+
 if __name__ == "__main__":
-    train()
-    if size == "small":
+    argument_parser = ArgumentParser(
+        description="Extract the decoder of byT5 and pre-train it for language modeling."
+    )
+    argument_parser.add_argument(
+        "--model_size",
+        choices=["small", "base", "large", "xl", "xxl"],
+        default="small",
+        help="specify which byt5 model size to extract the decoder from",
+    )
+    argument_parser.add_argument(
+        "--out_dir",
+        default="models",
+        help="directory where to write the model files",
+    )
+    args = argument_parser.parse_args()
+    base_model = f"google/byt5-{args.model_size}"
+    output_dir = join(
+        args.out_dir, f"bygpt5-{byt5_size_to_bygpt5_size(args.model_size)}"
+    )
+
+    train(base_model, output_dir)
+    if args.model_size == "small":
         # contrast reusing weights with training from scratch
-        train(from_scratch=True)
+        train(base_model, output_dir, from_scratch=True)
