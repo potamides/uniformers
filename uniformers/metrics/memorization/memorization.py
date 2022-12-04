a92d2559aeb60ba2f9b2788442030613a6a34058
Jonas Belouadi <jonas.belouadi@posteo.net>
2022-12-04 20:10:56 +0100

    fix(metrics): suppress reoccuring info messages

--- uniformers/metrics/memorization/memorization.py
+++ uniformers/metrics/memorization/memorization.py
@@ -3,11 +3,12 @@ from difflib import get_close_matches
 from datasets import Features, Value
 from datasets.info import MetricInfo
 import datasets.metric
+from datasets.utils.logging import get_verbosity, set_verbosity, set_verbosity_error
 
 class Memorization(datasets.metric.Metric):
     """Quantify memorization in a quatrain (based on https://arxiv.org/abs/2202.07646)"""
 
-    def __init__(self, train_data, cutoff=0.8, **kwargs):
+    def __init__(self, train_data, cutoff=0.7, **kwargs):
         super().__init__(**kwargs)
 
         self.train_data = train_data
@@ -27,12 +28,20 @@ class Memorization(datasets.metric.Metric):
             ),
         )
 
+    def _silent(self, func, *args, **kwargs):
+        old_verbosity = get_verbosity()
+        set_verbosity_error()
+        res = func(*args, **kwargs)
+        set_verbosity(old_verbosity)
+        return res
+
     def _compute(self, quatrains, schemes, meters, levels):
         styles, num_copied = ["rhyme", "meter", "alliteration"], 0
 
         for quatrain, scheme, meter, level in zip(quatrains, schemes, meters, levels):
             spl = dict(zip(styles, [scheme, meter, level]))
-            candidates = self.train_data.filter(lambda ex: all(ex[s] == spl[s] for s in styles))
+            # quickfix: this would produce a ton of log messages in this loop, so silence them temporarily
+            candidates = self._silent(self.train_data.filter, lambda ex: all(ex[s] == spl[s] for s in styles))
             assert len(candidates) > 0
             if get_close_matches(quatrain, ["\n".join(q) for q in candidates["text"]], n=1, cutoff=self.cutoff):
                 num_copied += 1
